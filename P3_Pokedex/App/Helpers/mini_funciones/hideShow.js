ac8fbc84dd1f0a51c69e2252a1a5a2bf02258042
Santino Boscatto <santinoboscatto05@gmail.com>
2024-10-08 17:39:02 -0300

    Actualizacion

--- P3_Pokedex/App/Helpers/mini_funciones/hideShow.js
+++
@@ -1,6 +0,0 @@
-export function ocultarMostrar(back, next, d){     
-    if(back===null) d.querySelector(".back").classList.add("none")
-    else d.querySelector(".back").classList.remove("none")
-    if(next===null) d.querySelector(".next").classList.add("none")
-    else d.querySelector(".next").classList.remove("none")
-}
--- P3_Pokedex/App/Helpers/mini_funciones/spriteVersions.js
+++
@@ -1,4 +0,0 @@
-export function spriteVersion(e){
-    let img = e.target.closest("div").previousElementSibling.previousElementSibling.previousElementSibling
-    img.setAttribute("src", e.target.getAttribute("data-sprite"))
-}
